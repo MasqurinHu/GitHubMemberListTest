63c38bb7562fd46224df70eda7d1c3326f3a4e49
MasqurinHu <Masqurin@icloud.com>
2020-02-20 11:26:19 +0800

    add user info model

---
+++ GitHubMemberListTest/Model/UserInfoModel.swift
@@ -0,0 +1,19 @@
+//
+//  UserInfoModel.swift
+//  GitHubMemberListTest
+//
+//  Created by 五加一 on 2020/2/20.
+//  Copyright © 2020 五加一. All rights reserved.
+//
+
+import Foundation
+
+struct UserInfoModel: Codable {
+    
+    let avatarUrl: String?
+    let name: String?
+    let bio: String?
+    let login: String?
+    let location: String?
+    let blog: String?
+}
